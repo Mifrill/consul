270cc95cff6705723ff1e0a6404fd68dad4386f5
Daniel Nephin <dnephin@hashicorp.com>
2021-02-22 13:14:15 -0500

    submatview: setup the interface for the store

--- agent/submatview/store.go
+++ agent/submatview/store.go
@@ -5,44 +5,75 @@ import (
 	"fmt"
 	"sync"
 	"time"
+
+	"github.com/hashicorp/consul/agent/cache"
+	"github.com/hashicorp/consul/lib/ttlcache"
 )
 
 type Store struct {
-	lock  sync.RWMutex
-	byKey map[string]*Materializer
+	lock       sync.RWMutex
+	byKey      map[string]*Materializer
+	expiryHeap *ttlcache.ExpiryHeap
 }
 
 func NewStore() *Store {
-	return &Store{byKey: make(map[string]*Materializer)}
+	return &Store{
+		byKey:      make(map[string]*Materializer),
+		expiryHeap: ttlcache.NewExpiryHeap(),
+	}
 }
 
-func (s *Store) Fetch(ctx context.Context, opts FetchOptions) (FetchResult, error) {
+// Get a value from the store, blocking if the store has not yet seen the
+// req.Index value.
+// See agent/cache.Cache.Get for complete documentation.
+func (s *Store) Get(
+	ctx context.Context,
+	typ string,
+	req cache.Request,
+) (result interface{}, meta cache.ResultMeta, err error) {
+	return nil, cache.ResultMeta{}, nil
+}
+
+// Notify the updateCh when there are updates to the entry identified by req.
+// See agent/cache.Cache.Notify for complete documentation.
+func (s *Store) Notify(
+	ctx context.Context,
+	typ string,
+	req cache.Request,
+	correlationID string,
+	updateCh chan<- cache.UpdateEvent,
+) error {
+	return nil
+}
+
+func (s *Store) getMaterializer(opts GetOptions) *Materializer {
+	// TODO: use makeEntryKey
+	var key string
+
 	s.lock.RLock()
-	mat, ok := s.byKey[opts.Key]
+	mat, ok := s.byKey[key]
 	s.lock.RUnlock()
 
-	if !ok {
-		s.lock.Lock()
-		mat, ok := s.byKey[opts.Key]
-		if !ok {
-			mat = opts.NewMaterializer()
-			s.byKey[opts.Key] = mat
-		}
-		s.lock.Unlock()
+	if ok {
+		return mat
 	}
 
-	// TODO: track last used for expiration
-	ctx, cancel := context.WithTimeout(ctx, opts.Timeout)
-	defer cancel()
-	return mat.Fetch(ctx, opts)
+	s.lock.Lock()
+	mat, ok = s.byKey[key]
+	if !ok {
+		mat = opts.NewMaterializer()
+		s.byKey[opts.Key] = mat
+	}
+	s.lock.Unlock()
+	return mat
 }
 
-// makeEntryKey matches agent/cache.makeEntryKey
+// makeEntryKey matches agent/cache.makeEntryKey, but may change in the future.
 func makeEntryKey(t, dc, token, key string) string {
 	return fmt.Sprintf("%s/%s/%s/%s", t, dc, token, key)
 }
 
-type FetchOptions struct {
+type GetOptions struct {
 	// TODO: needs to use makeEntryKey
 	Key string
 
